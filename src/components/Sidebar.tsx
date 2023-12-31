45d28ea5ba0e92ff12e295388db297e3cf036bf1
marosca <maria.moreno@masmovil.com>
2023-12-31 07:29:18 +0100

    SSR

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import Image from 'next/image';
+import {
+  IoBrowsersOutline,
+  IoCalculator,
+  IoFootball,
+  IoLogoReact,
+} from 'react-icons/io5';
+import SidebarMenuItem from './SidebarMenuItem';
+
+const menuItems = [
+  {
+    path: '/dashboard/main',
+    icon: <IoBrowsersOutline sizew={40} />,
+    title: 'Dashboard',
+    subtitle: 'Visualización',
+  },
+  {
+    path: '/dashboard/counter',
+    icon: <IoCalculator sizew={40} />,
+    title: 'Counter',
+    subtitle: 'Contador client side',
+  },
+  {
+    path: '/dashboard/pokemons',
+    icon: <IoFootball sizew={40} />,
+    title: 'Pokemons',
+    subtitle: 'Generación estática',
+  },
+];
+export const Sidebar = () => {
+  return (
+    <div
+      style={{ width: '400px' }}
+      id='menu'
+      className='bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll'
+    >
+      <div id='logo' className='my-4 px-6'>
+        <h1 className='text-lg md:text-2xl font-bold text-white flex items-center'>
+          <IoLogoReact className='mr-2' />
+          <span>Dash</span>
+          <span className='text-blue-500'>8</span>.
+        </h1>
+        <p className='text-slate-500 text-sm'>
+          Manage your actions and activities
+        </p>
+      </div>
+
+      <div id='profile' className='px-6 py-10'>
+        <p className='text-slate-500'>Welcome back,</p>
+        <a href='#' className='inline-flex space-x-2 items-center'>
+          <span>
+            <Image
+              className='rounded-full w-8 h-8'
+              width={50}
+              height={50}
+              src='http://images.unsplash.com/photo-1542909168-82c3e7fdca5c'
+              alt=''
+            />
+          </span>
+          <span className='text-sm md:text-base font-bold'>Edward Tompson</span>
+        </a>
+      </div>
+
+      <div id='nav' className='w-full px-6'>
+        {menuItems.map((menuItem) => (
+          <SidebarMenuItem
+            key={menuItem.title}
+            path={menuItem.path}
+            icon={menuItem.icon}
+            title={menuItem.title}
+            subtitle={menuItem.subtitle}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/pokemons/components/PokemonCard.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { SimplePokemon } from '..';
+import Image from 'next/image';
+import Link from 'next/link';
+import { IoHeartOutline } from 'react-icons/io5';
+
+interface Props {
+  pokemon: SimplePokemon;
+}
+export const PokemonCard = ({ pokemon }: Props) => {
+  return (
+    <div className='mx-auto right-0 mt-2 w-60'>
+      <div className='bg-white rounded overflow-hidden shadow-lg'>
+        <div className='text-center p-6 bg-gray-800 border-b items-center flex flex-col'>
+          <Image
+            key={pokemon.id}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+            width={100}
+            height={100}
+            alt={pokemon.name}
+            priority={false}
+          />
+          <p className='pt-2 text-lg font-semibold text-gray-50 capitalize'>
+            {pokemon.name}
+          </p>
+          <p className='text-sm text-gray-100'>John@Doe.com</p>
+          <div className='mt-5'>
+            <Link
+              href={`/dashboard/pokemons/pokemon/${pokemon.id}`}
+              className='border rounded-full py-2 px-4 text-xs font-semibold text-gray-100'
+            >
+              Más información
+            </Link>
+          </div>
+        </div>
+        <div className='border-b'>
+          <Link
+            href='/dashboard/main'
+            className='px-4 py-2 hover:bg-gray-100 flex'
+          >
+            <div className='text-red-600'>
+              <IoHeartOutline />
+            </div>
+            <div className='pl-3'>
+              <p className='text-sm font-medium text-gray-800 leading-none'>
+                No es favorito
+              </p>
+              <p className='text-xs text-gray-500'>View your campaigns</p>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
